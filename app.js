592dfbecb3d0997c66c21f1fb8cc610a525ce4b6
nouroz khan <nouroz.khan1.nk@gmail.com>
2017-01-12 22:19:26 +0500

    task compeleted

---
+++ app.js
@@ -0,0 +1,75 @@
+//function for calculating time of quiz
+var sec = 59;
+
+function start()
+{
+    document.getElementById("qs").className = "hide";
+    document.getElementById("q1").className = "show";
+    var stop = setInterval(quizTime,1000);
+    function quizTime()
+{
+    document.getElementById("time").innerHTML = "Time : "+sec;
+    sec = sec - 1;
+    if(sec == 0)
+    {
+        document.getElementById("q1").className = "hide";
+        document.getElementById("q2").className = "hide";
+        document.getElementById("q3").className = "hide";
+        document.getElementById("q4").className = "hide";
+        document.getElementById("q5").className = "hide";
+        result();
+        clearInterval(stop);
+        document.getElementById("time").className = "hide";
+    }
+    else
+    {
+    return sec;
+    }
+}
+}
+
+//declreation of global variables
+var score = 0;
+var j = 1;
+var s = "";
+var rightAnswers = ["1985","abrani","13","iskander mirza","Liaquat Ali Khan"];
+//function for checking answer 
+function check()
+{
+   var a = document.getElementsByName("ans");
+   for(var i = 0 ; i < a.length ; i++)
+   {
+       if(a[i].checked)
+       {
+           s = a[i].value;
+           for(k = 0 ; k < rightAnswers.length ; k++)
+
+            if(s == rightAnswers[k])
+            {
+                //adding numbers to your final result
+                score++;
+                a[i].value = "wrong";
+            }
+        }
+    }
+    //for changing the question 
+    document.getElementById("q"+j).className = "hide";
+    j = j + 1;
+    document.getElementById("q"+j).className = "show";  
+       
+   
+}
+// function to display result
+function result()
+{
+    document.getElementById("q6").className = "hide";
+    document.getElementById("time").className = "hide";
+    var ruku = sec;
+    document.getElementById("head").className = "hide";
+    document.getElementById("main").style.border = "2px solid #967c82";
+    var correct = score;
+    var inCorrect = 5 - score;
+    var perc = (correct*100)/5;
+    document.getElementById("result").innerHTML = "<h1>Result</h1>correct answers = "+correct+"<br> InCorrect answers = "
+    +inCorrect+"<br> percentage is " +perc+ "% <br> Time Duration : "+ruku ;
+}
